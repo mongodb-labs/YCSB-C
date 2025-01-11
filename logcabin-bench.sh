48a1ba401827c31ac401c0b68dce853869150671
A. Jesse Jiryu Davis <jesse@mongodb.com>
2025-01-11 17:47:59 +0000

    update logcabin-bench.sh

--- logcabin-bench.sh
+++ logcabin-bench.sh
@@ -11,6 +11,7 @@ serverId = 1
 listenAddresses = $SERVER1
 clusterUUID = foo
 storagePath = storage1
+logPolicy = NOTICE
 EOF
 
 cat <<EOF > ~/logcabin/conf2.conf
@@ -18,6 +19,7 @@ serverId = 2
 listenAddresses = $SERVER2
 clusterUUID = foo
 storagePath = storage2
+logPolicy = NOTICE
 EOF
 
 cat <<EOF > ~/logcabin/conf3.conf
@@ -25,49 +27,57 @@ serverId = 3
 listenAddresses = $SERVER3
 clusterUUID = foo
 storagePath = storage3
+logPolicy = NOTICE
 EOF
 
 sleep 5
 
-echo SERVER 1 ================================
+echo 'SERVER 1 ================================'
 
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/jesse-2024.pem $SERVER1 <<EOF
+ssh -v -o StrictHostKeyChecking=no -i ~/.ssh/jesse-2024.pem $SERVER1 <<EOF
+set +x
 cd logcabin
 killall -9 perf LogCabin Reconfigure || true
 rm -rf storage1 1.log
 ./build/LogCabin --config conf1.conf --bootstrap
 echo DONE BOOTSTRAPPING
-nohup perf record -o perf1.data --call-graph=dwarf ./build/LogCabin --config conf1.conf --log 1.log >1.out 2>&1 </dev/null & 
+#nohup perf record -o perf1.data --call-graph=dwarf ./build/LogCabin --config conf1.conf --log 1.log >1.out 2>&1 </dev/null & 
+./build/LogCabin --config conf1.conf --log 1.log >1.out 2>&1 </dev/null & 
 ps aux | grep LogCabin
 EOF
 
-echo SERVER 2 ================================
+echo 'SERVER 2 ================================'
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/jesse-2024.pem $SERVER2 <<EOF
+set +x
 cd logcabin
 killall -9 perf LogCabin Reconfigure || true
 rm -rf storage2 2.log
-nohup perf record -o perf2.data --call-graph=dwarf ./build/LogCabin --config conf2.conf --log 2.log >2.out 2>&1 </dev/null & 
+#nohup perf record -o perf2.data --call-graph=dwarf ./build/LogCabin --config conf2.conf --log 2.log >2.out 2>&1 </dev/null & 
+./build/LogCabin --config conf2.conf --log 2.log >2.out 2>&1 </dev/null & 
 ps aux | grep LogCabin
 EOF
 
-echo SERVER 3 ================================
+echo 'SERVER 3 ================================'
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/jesse-2024.pem $SERVER3 <<EOF
+set +x
 cd logcabin
 killall -9 perf LogCabin Reconfigure || true
 rm -rf storage3 3.log
-nohup perf record -o perf3.data --call-graph=dwarf ./build/LogCabin --config conf3.conf --log 3.log >3.out 2>&1 </dev/null & 
+#nohup perf record -o perf3.data --call-graph=dwarf ./build/LogCabin --config conf3.conf --log 3.log >3.out 2>&1 </dev/null & 
+./build/LogCabin --config conf3.conf --log 3.log >3.out 2>&1 </dev/null & 
 ps aux | grep LogCabin
 EOF
 
 sleep 5
 
-echo RECONFIGURE ==============================
+echo 'RECONFIGURE =============================='
 
 ~/logcabin/build/Examples/Reconfigure --cluster=$SERVER1 set $SERVER1 $SERVER2 $SERVER3
 ~/logcabin/build/Examples/HelloWorld --cluster=$SERVER1,$SERVER2,$SERVER3
-LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./ycsbc -db logcabin -threads 1 -P ./workloads/workloada.spec -host $SERVER1,$SERVER2,$SERVER3
+
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./ycsbc -db logcabin -threads 256 -P ./workloads/workloada.spec -host $SERVER1,$SERVER2,$SERVER3 | tee "$(date +%s).log"
 
 sleep 5
 
